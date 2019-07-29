642ea0869039b7cf10d31e99cde459038ee84c98
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-07-29 16:59:13 +0800

    add return status code and response

--- app/graphql/mutations/create_user.rb
+++ app/graphql/mutations/create_user.rb
@@ -2,22 +2,26 @@ module Mutations
   # mutation for creating new user
   class CreateUser < BaseMutation
     description "Create User"
+
     # base input object for email and password
     class AuthProviderSignupData < Types::BaseInputObject
       argument :email, Types::AuthProviderEmailInput, required: false
     end
-  
-    # argument :name, String, required: true
     argument :auth_provider, AuthProviderSignupData, required: false
     argument :user_type, Types::UserEnum, required: false
-    
-    type Types::UserType
-   
+    field :response, String, null: true
+    field :status_code, Integer, null: false
+    # type Types::UserType
     def resolve(auth_provider: nil, user_type: nil)
-      User.create!(
-        email: auth_provider&.[](:email)&.[](:email),
-        password: auth_provider&.[](:email)&.[](:password)
-      )
+      if context[:current_user].nil?
+        { response: "Unauthorized", status_code: 401 }
+      else
+        User.create!(
+          email: auth_provider&.[](:email)&.[](:email),
+          password: auth_provider&.[](:email)&.[](:password)
+        )
+        { response: "User Created", status_code: 200 }
+      end
     end
   end
 end
