0f4ab40ee2ee915dd5c507b8b3487d5e2ebb8e89
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-07-29 16:58:39 +0800

    add return status code and response

--- app/graphql/mutations/delete_user.rb
+++ app/graphql/mutations/delete_user.rb
@@ -3,12 +3,19 @@ module Mutations
   class DeleteUser < BaseMutation
     description "Delete User"
     argument :id, Int, required: true
+    field :response, String, null: true
+    field :status_code, Integer, null: false
 
     type Types::UserType
 
     def resolve(id: nil)
-      user = User.find(id)
-      user.delete
+      if context[:current_user].nil?
+        { response: "Unauthorized", status_code: 401 }
+      else
+        user = User.find(id)
+        user.delete
+        { response: "User Deleted", status_code: 200 }
+      end
     end
   end
 end
