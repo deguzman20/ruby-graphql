0aa96311ecb9df4e3a6636f8d96a677eb97ea9ab
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-07-29 16:58:59 +0800

    add return status code and response

--- app/graphql/mutations/create_vote.rb
+++ app/graphql/mutations/create_vote.rb
@@ -3,16 +3,24 @@ module Mutations
   class CreateVote < BaseMutation
     description "Create Vote"
     argument :link_id, ID, required: false
+    field :response, String, null: true
+    field :status_code, Integer, null: false
 
     type Types::VoteType
 
     def resolve(link_id: nil)
-      Vote.create!(
-        link: Link.find(link_id),
-        user: context[:current_user]
-      )
+      if context[:current_user].nil?
+        { response: "Unauthorized", status_code: 401 }
+      else
+        Vote.create!(
+          link: Link.find(link_id),
+          user: context[:current_user]
+        )
+        { response: "Vote Created", status_code: 200 }
+      end
     rescue ActiveRecord::RecordInvalid => e
-      GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
+      error = e.record.full_messages
+      GraphQL::ExecutionError.new("Invalid input: #{error}")
     end
   end
 end
