38a51f26da9547dc7d060a4bcaa30c892ac74bd6
deguzman20 <alejandrogacumadeguzman@gmail.com>
2019-07-29 16:58:47 +0800

    add return status code and response

--- app/graphql/mutations/delete_link.rb
+++ app/graphql/mutations/delete_link.rb
@@ -3,11 +3,18 @@ module Mutations
   class DeleteLink < BaseMutation
     description "Delete Link"
     argument :id, Int, required: true
+    field :response, String, null: true
+    field :status_code, Integer, null: false
 
     type Types::LinkType
     def resolve(id: nil)
-      link = Link.find(id)
-      link.delete
+      if context[:current_user].nil?
+        { response: "Unauthorized", status_code: 401 }
+      else
+        link = Link.find(id)
+        link.delete
+        { response: "Link Deleted", status_code: 200 }
+      end
     end
   end
 end
